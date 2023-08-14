12fc8ee8aa40709bafa34aa853445cb2640d225d
nyakiomwangi <cnyakio@usiu.ac.ke>
2023-08-14 15:19:39 +0300

    Coonected project to sqlite database..installed nuget packaging manager

--- Program.cs
+++ Program.cs
@@ -1,81 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 class Program
 {
-    
     static void Main()
     {
-      
+        using (var context = new QuizDbContext())
+        {
+            InitializeQuizQuestions(context);
 
-
-            List<Question> quizQuestions = InitializeQuizQuestions();
-
-        Console.WriteLine("Welcome to the Quiz Application!");
-            
-
-
+            Console.WriteLine("Welcome to the Quiz Application!");
             Console.WriteLine("Answer the following questions:\n");
 
-        int score = 0;
-        foreach (var question in quizQuestions)
-        {
-            Console.WriteLine(question.QuestionText);
-            for (int i = 0; i < question.Options.Length; i++)
+            int totalScore = 0;
+            var quizQuestions = context.Questions.ToList();
+
+            foreach (var question in quizQuestions)
             {
-                Console.WriteLine($"{i + 1}. {question.Options[i]}");
+                Console.WriteLine(question.QuestionText);
+                for (int i = 0; i < question.Options.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {question.Options[i]}");
+                }
+
+                Console.Write("Your Answer (Please Key In one of the option numbers ): ");
+                int userAnswer;
+                while (!int.TryParse(Console.ReadLine(), out userAnswer) || userAnswer < 1 || userAnswer > question.Options.Length)
+                {
+                    Console.Write("Invalid input. Please enter a valid option number: ");
+                }
+
+                if (question.IsCorrect(userAnswer))
+                {
+                    Console.WriteLine("Correct!\n");
+                    totalScore++;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect. The correct answer is: {question.GetCorrectAnswer()}\n");
+                }
             }
 
-            Console.Write("Your Answer (Please Key In one of the option numbers ): ");
-            int userAnswer;
-            while (!int.TryParse(Console.ReadLine(), out userAnswer) || userAnswer < 1 || userAnswer > question.Options.Length)
-            {
-                Console.Write("Invalid input. Please enter a valid option number: ");
-            }
-
-            if (question.IsCorrect(userAnswer))
-            {
-                Console.WriteLine("Correct!\n");
-                score++;
-            }
-            else
-            {
-                Console.WriteLine($"Incorrect. The correct answer is: {question.GetCorrectAnswer()}\n");
-            }
+            Console.WriteLine($"Quiz completed! Your final score: {totalScore} out of {quizQuestions.Count}");
         }
-
-        Console.WriteLine($"Quiz completed! Your final score: {score} out of {quizQuestions.Count}");
     }
 
-    
-
-
-    static List<Question> InitializeQuizQuestions()
+    static void InitializeQuizQuestions(QuizDbContext context)
     {
-        List<Question> quizQuestions = new List<Question>();
+        context.Database.EnsureCreated();
 
-        // Add your quiz questions here
-        quizQuestions.Add(new Question("What is the capital of France?", new string[] { "Berlin", "Paris", "London", "Rome" }, 2));
-        quizQuestions.Add(new Question("Which planet is known as the 'Red Planet'?", new string[] { "Mars", "Jupiter", "Venus", "Saturn" }, 1));
-        // Add more questions...
+        if (!context.Questions.Any())
+        {
+            context.Questions.Add(new QuestionEntity
+            {
+                QuestionText = "What is the capital of France?",
+                Options = new string[] { "Berlin", "Paris", "London", "Rome" },
+                CorrectAnswer = 2
+            });
 
-        return quizQuestions;
+            context.Questions.Add(new QuestionEntity
+            {
+                QuestionText = "Which planet is known as the 'Red Planet'?",
+                Options = new string[] { "Mars", "Jupiter", "Venus", "Saturn" },
+                CorrectAnswer = 1
+            });
+
+            context.SaveChanges();
+        }
     }
 }
 
-class Question
+class QuizDbContext : DbContext
 {
-    public string QuestionText { get; }
-    public string[] Options { get; }
-    public int CorrectAnswer { get; }
+    public DbSet<QuestionEntity> Questions { get; set; }
 
-    public Question(string questionText, string[] options, int correctAnswer)
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        QuestionText = questionText;
-        Options = options;
-        CorrectAnswer = correctAnswer;
+        optionsBuilder.UseSqlite("Data Source=quiz.db");
     }
+}
+
+class QuestionEntity
+{
+    public int Id { get; set; }
+    public required string QuestionText { get; set; }
+    public required string[] Options { get; set; }
+    public int CorrectAnswer { get; set; }
 
     public bool IsCorrect(int userAnswer)
     {
@@ -87,6 +106,3 @@ class Question
         return Options[CorrectAnswer - 1];
     }
 }
-
-
-
